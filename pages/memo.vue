437bbc938c3004cd813e7209d19728d6b9b8c4da
TC Wang <wangtct@hotmail.com>
2024-05-05 15:16:23 +0800

    l001

---
+++ pages/memo.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container">
+    <div class="flex flex-row flex-wrap">
+      <div class="w-full md:w-1/2">
+        <h1>備忘錄</h1>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+useHead({
+  title: "備忘錄",
+});
+</script>
+
+<style></style>
