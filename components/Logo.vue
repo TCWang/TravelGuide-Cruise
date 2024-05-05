32c5a6597960bf93c86c53b73f5932162be4542a
TC Wang <wangtct@hotmail.com>
2024-05-05 14:41:23 +0800

    first commit

---
+++ components/Logo.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <div class="">
+      <NuxtLink class="font-bold text-white text-lg" to="/">郵輪旅遊</NuxtLink>
+    </div>
+  </div>
+</template>
+
+<script></script>
+
+<style scoped></style>
---
+++ pages/index.vue
@@ -0,0 +1,96 @@
+<template>
+  <div class="container">
+    <div class="flex flex-row flex-wrap">
+      <div class="w-full md:w-1/2">
+        <h1>郵輪旅遊</h1>
+        <h2>郵輪旅遊網站</h2>
+        <ul>
+          <li>
+            <a href="https://www.vacationstogo.com/">Vacations To Go</a> -
+            最完整的郵輪資訊整合網站！ 可以作為比價入口網站
+          </li>
+          <li>
+            <a href="https://www.expediacruises.ca/en-CA/100017"
+              >Expedia Cruises</a
+            >
+            主要 旅遊目的地
+            <ul class="list-inline">
+              <li>
+                <a
+                  href="https://www.shoreexcursionsgroup.com/caribbean-shore-excursions?id=26554&data=richmond@expediacruises.com&source=website"
+                  >Caribbean</a
+                >
+                加勒比地區,
+              </li>
+              <li>
+                <a
+                  href="https://www.shoreexcursionsgroup.com/bahamas-shore-excursions"
+                  >Bahamas</a
+                >
+                巴哈馬,
+              </li>
+              <li>
+                <a
+                  href="https://www.shoreexcursionsgroup.com/mexican-riviera-shore-excursions?id=26554&data=richmond@expediacruises.com&source=website"
+                  >Mexico</a
+                >
+                墨西哥,
+              </li>
+              <li>
+                <a
+                  href="https://www.shoreexcursionsgroup.com/alaska-shore-excursions?id=26554&data=richmond@expediacruises.com&source=website"
+                  >Alaska</a
+                >
+                阿拉斯加州,
+              </li>
+              <li>
+                <a
+                  href="https://www.shoreexcursionsgroup.com/europe-shore-excursions?id=26554&data=richmond@expediacruises.com&source=website"
+                  >Europe</a
+                >
+                歐洲,
+              </li>
+              <li>
+                <a
+                  href="https://www.shoreexcursionsgroup.com/bermuda-shore-excursions"
+                  >Bermuda</a
+                >
+                百慕達,
+              </li>
+              <li>
+                <a
+                  href="https://www.shoreexcursionsgroup.com/hawaii-shore-excursions?id=26554&data=richmond@expediacruises.com&source=website"
+                  >Hawaii</a
+                >
+                夏威夷
+              </li>
+            </ul>
+          </li>
+
+          <li>
+            Taoticket <a href="https://www.taoticket.ca/">踏鷗郵輪</a>,
+            主要目的地
+            <ul class="list-inline">
+              <li>Caribbean 加勒比地區,</li>
+              <li>World Cruises 世界郵輪,</li>
+              <li>Alaska 阿拉斯加州,</li>
+              <li>United States 美國 & Mexico 墨西哥,</li>
+              <li>Hawaii 夏威夷</li>
+              <li>North America 北美,</li>
+              <li>Ocean Cruises 遠洋郵輪</li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+      <div class="w-full md:w-1/2"></div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+useHead({
+  title: "郵輪旅遊",
+});
+</script>
+
+<style></style>
